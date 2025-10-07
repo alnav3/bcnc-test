6015dfe38b91407a80acd62553e34914934d0ff9
alex <developer@alnav.dev>
2025-10-07 19:07:34 +0200

    feat: add JPA repository interfaces for data access

---
+++ src/main/java/com/bcnc/bcnc/infrastructure/repository/JpaProductRepository.java
@@ -0,0 +1,9 @@
+package com.bcnc.bcnc.infrastructure.repository;
+
+import com.bcnc.bcnc.infrastructure.entity.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JpaProductRepository extends JpaRepository<ProductEntity, Long> {
+}
